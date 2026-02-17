0b7ce2a2ebd6eb119230a98198c66f6118bf4873
Kava Himanshu <kavahimanshu@Kavas-MacBook-Air.local>
2026-02-17 13:45:30 +0530

    s

--- src/main/java/com/main/Security/WebConfig.java
+++ src/main/java/com/main/Security/WebConfig.java
@@ -3,14 +3,12 @@ package com.main.Security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-          .addResourceHandler("/images/**")
-          .addResourceLocations("file:/Users/kavahimanshu/images/");
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:" + System.getProperty("user.dir") + "/images/");
     }
 }
